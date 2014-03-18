74baf23b3e31f8050e1ac222ff9b5f56daa9ecba
Clinton Blackburn <cblackburn@kyru.us>
2014-03-18 10:40:47 -0400

    Corrected Privacy Policy URL Bug

--- legal/urls.py
+++ legal/urls.py
@@ -4,7 +4,7 @@ from legal.views import PrivacyPolicyView, TermsOfServiceView, TermsOfServiceAcc
 
 urlpatterns = patterns(
     '',
-    url(r'^privacy-policy/$', PrivacyPolicyView.as_view, name='privacy_policy'),
+    url(r'^privacy-policy/$', PrivacyPolicyView.as_view(), name='privacy_policy'),
     url(r'^terms-of-service/$', TermsOfServiceView.as_view(), name='tos'),
     url(r'terms-of-service-accept/$', TermsOfServiceAcceptView.as_view(), name='tos_accept'),
 )
